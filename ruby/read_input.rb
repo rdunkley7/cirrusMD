2925da12ae125c3c46c952c24e30f51f658b4289
rdunkley7 <rachel.dunkley@sportsengine.com>
2021-09-09 12:12:40 -0500

    add date validation

--- ruby/read_input.rb
+++ ruby/read_input.rb
@@ -1,5 +1,6 @@
 class ReadInput
   require 'csv'
+  require 'date'
 
   def initialize
     if File.exist?("input.csv")
@@ -14,31 +15,36 @@ class ReadInput
     data.each do |patient|
       first_name = patient[:first_name]
       last_name = patient[:last_name]
-      patient[:dob] = patient[:dob]
+      patient[:dob] =  validate_date(patient[:dob].to_s)
       member_id = patient[:member_id]
       patient[:effective_date] = patient[:effective_date]
       patient[:expiry_date] = patient[:expiry_date]
       patient[:phone_number] = validate_phone(patient[:phone_number].to_s)
 
 
-      puts patient
-
-      if patient[:first_name].nil? || patient[:last_name].nil?
+       if patient[:first_name].nil? || patient[:last_name].nil? || patient[:dob].nil? || patient[:member_id].nil? || patient[:effective_date].nil?
         invalid_data.push(patient)
       else
         valid_data.push(patient)
       end
     end
-
-    puts invalid_data
-    puts '*valid', valid_data
-
-    # output()
+    #output_csv(valid_data, invalid_data)
   end
+
+
+   def validate_date(date)
+    begin
+      date = Date.strptime(date).strftime("%Y-%m-%d")
+      return date
+    rescue
+      return nil
+    end
+   end
 
   def validate_phone(number)
     return number.gsub(/\A(\+)|\D+/, '\1')
   end
+
 
 
 
